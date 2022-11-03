34dfda0da67a88acfa29114caf2ca78c2d79fcd9
TheJoSam <65750856+TheJoSam@users.noreply.github.com>
2022-11-03 19:41:27 +0100

    Delete beta-features directory
    Signed-off-by: TheJoSam <65750856+TheJoSam@users.noreply.github.com>

--- beta-features/embed_converter/embed_converter.py
+++
@@ -1,9 +0,0 @@
-import discord
-import json
-
-def get_embed(embed_file: str):
-    pass
-
-
-if __name__ == '__main__':
-    get_embed('beta-features/test_embed.json')
--- beta-features/readme.md
+++
@@ -1,3 +0,0 @@
-# Beta Features
-
-Here are features tha are currently in development and will be added in next iterrations
