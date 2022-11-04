67a63db717e9734211292aff68592b42711016e6
TheJoSam <65750856+TheJoSam@users.noreply.github.com>
2022-11-04 18:07:01 +0100

    Added files via upload

--- extensions/command_line/main.py
+++ extensions/command_line/main.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
 from discord.ext import tasks
+import asyncio
 import threading
 import os
 import sys
 from extensions.command_line.commands import help, inject
 
+global task_1
 global command_file
 global command_list
 command_list = []
@@ -18,16 +20,24 @@ for i in os.listdir('extensions/command_line/commands'): # using commands direct
         continue
     else: command_list.append(command)
 
-def bot_command_line():
-    while True:
-        command = input('> ')
-        if command.startswith('help'):
-            help.call(command_list)
-        elif command.startswith('inject'):
-            inject.call(command_file)
+
+async def bot_command_line():
+        while True:
+            command = input('> ')
+            if command.startswith('help'):
+                help.call(command_list)
+            elif command.startswith('inject'):
+                inject.call(command_file)
+
 
 class command_line(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    #command_line_thread = threading.Thread(target=bot_command_line(), daemon=True)
+
+
+def setup(bot):
+    bot.add_cog(command_line(bot))
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(bot_command_line())
+    loop.run_until_complete(task)
+
